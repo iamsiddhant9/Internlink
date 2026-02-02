0be3c3aa74450ca919b730d8a01c61c0aaea5809
Siddhant Sonarkar <siddhantsonarkar9@gmail.com>
2026-02-02 10:29:50 +0530

    Browser router

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Register from "./pages/Register.jsx";
 
 function App(){
   return(
-    <BrowserRouter>
+    
     <Routes>
       <Route path="/login" element={<Login/>}></Route>
       <Route  path="/register" element={<Register/>}></Route>
     </Routes>
-    </BrowserRouter>
+    
   );
 }
 export default App;
